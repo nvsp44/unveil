7c24ff4c3a29855d759c894f83756a7716235ca8
A200034369 <venkata.sivaprasad@t-systems.com>
2024-08-23 12:17:52 +0530

    Initial commit

---
+++ src/test/java/com/intrepid/unveil/UnveilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intrepid.unveil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnveilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
